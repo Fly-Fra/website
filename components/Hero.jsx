b63123f2c44b545b08df7fca9394b564956eb3c4
Fly-Fra <flyfraacedemy@gmail.com>
2025-02-09 09:31:24 +0000

    HERO IMAGE

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -33,17 +33,25 @@ const Hero = ({setVisible}) => {
     start();
   }, []);
   const slides = [
+    { ico: "/IMG-EMIRATES.jpg", pos: "object-center", width: "1112", height: "1280" },
+    { ico: "/20211021_1903.jpg", pos: "object-top", width: "1200", height: "1408" },
+    { ico: "/IMG-7e65152716121ff21d04cd8065447dae-V.jpg", pos: "object-top", width: "1200", height: "1408" },
+    { ico: "/20200109_164445.jpg", pos: "object-top", width: "1200", height: "1408" },
+    { ico: "/IMG_2015.jpg", pos: "object-top", width: "1200", height: "1408" },
     
+    { ico: "/IMG_3964.jpg", pos: "object-top", width: "1200", height: "1408" },
     { ico: "/IMG_3776.jpg", pos: "object-top", width: "99999999", height: "9999999" },
     { ico: "/fss05.jpeg", pos: "object-top", width: "99999999", height: "9999999" },
     { ico: "/cover2.jpg", pos: "object-top", width: "564", height: "748" },
     { ico: "/ckp.jpg", pos: "object-top", width: "1200", height: "1408" },
-    { ico: "/language.jpg", pos: "object-bottom", width: "1750", height: "2184" },
+
     { ico: "/cover.jpg", pos: "object-center", width: "1112", height: "1280" },
-    { ico: "/plane.jpeg", pos: "object-bottom", width: "1000", height: "668" },
+
     { ico: "/agentdescale1-z.jpg", pos: "object-top", width: "564", height: "750" },
     { ico: "/agentdescale2.jpg", pos: "object-top", width: "373", height: "280" },
     { ico: "/agentdescale3.jpg", pos: "object-top", width: "99999999", height: "9999999" },
+
+
 
   ];
 
